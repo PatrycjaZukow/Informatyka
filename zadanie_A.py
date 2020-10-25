064eb9831a08d5917702c3a9ac145d91b8a60977
PatrycjaZukow <73012504+PatrycjaZukow@users.noreply.github.com>
2020-10-25 10:34:36 +0100

    Update zadanie_A.py

--- zadanie_A.py
+++ zadanie_A.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # Przedmiot: Informatyka
-# Kierunek studiów: ???
+# Kierunek studiów: Inżynieria transportu
 # Semestr: zimowy
 # Rok akademicki: 2020/2021
-# Data (dzień.miesiąc.rok): 31.11.2020
-# Imię: ???
-# Nazwisko: ???
-# Numer albumu ZUT: ???
+# Data (dzień.miesiąc.rok): 25.10.2020
+# Imię: Patrycja
+# Nazwisko: Żukowska
+# Numer albumu ZUT: 30971
 
 """
 W tym miejscu przepisać temat.
